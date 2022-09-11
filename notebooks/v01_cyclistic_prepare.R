a95b06223792a97a6c8db472f2d4c49a33898d7d
vitorhmf <vitorhmf@gmail.com>
2022-09-11 09:24:30 -0300

    r analysis

---
+++ notebooks/v01_cyclistic_prepare.R
@@ -0,0 +1,39 @@
+# 1. IMPORTS
+
+## 1.1 Libraries
+install.packages('readr')
+install.packages('dplyr')
+
+library('dplyr')
+library('readr')
+
+## 1.2 Loading Data
+trip_data_2021_08 <- read_csv('/home/vitor/Repos/google_project/data/202108-divvy-tripdata.csv')
+trip_data_2021_09 <- read_csv('/home/vitor/Repos/google_project/data/202109-divvy-tripdata.csv')
+trip_data_2021_10 <- read_csv('/home/vitor/Repos/google_project/data/202110-divvy-tripdata.csv')
+trip_data_2021_11 <- read_csv('/home/vitor/Repos/google_project/data/202111-divvy-tripdata.csv')
+trip_data_2021_12 <- read_csv('/home/vitor/Repos/google_project/data/202112-divvy-tripdata.csv')
+trip_data_2022_01 <- read_csv('/home/vitor/Repos/google_project/data/202201-divvy-tripdata.csv')
+trip_data_2022_02 <- read_csv('/home/vitor/Repos/google_project/data/202202-divvy-tripdata.csv')
+trip_data_2022_03 <- read_csv('/home/vitor/Repos/google_project/data/202202-divvy-tripdata.csv')
+trip_data_2022_04 <- read_csv('/home/vitor/Repos/google_project/data/202204-divvy-tripdata.csv')
+trip_data_2022_05 <- read_csv('/home/vitor/Repos/google_project/data/202205-divvy-tripdata.csv')
+trip_data_2022_06 <- read_csv('/home/vitor/Repos/google_project/data/202206-divvy-tripdata.csv')
+trip_data_2022_07 <- read_csv('/home/vitor/Repos/google_project/data/202207-divvy-tripdata.csv')
+
+## 1.3 Join
+df1 <- bind_rows(trip_data_2021_08, 
+                      trip_data_2021_09, 
+                      trip_data_2021_10, 
+                      trip_data_2021_11, 
+                      trip_data_2021_12,
+                      trip_data_2022_01,
+                      trip_data_2022_02,
+                      trip_data_2022_03,
+                      trip_data_2022_04,
+                      trip_data_2022_05,
+                      trip_data_2022_06,
+                      trip_data_2022_07)
+
+## 1.4 Export Raw Data
+write.csv(df1,"/home/vitor/Repos/google_project/data/df_raw_cyclistic.csv", row.names = FALSE)
---
+++ notebooks/v02_cyclistic_process.R
@@ -0,0 +1,118 @@
+# 1. IMPORTS
+
+## 1.1 Libraries
+install.packages('readr')
+install.packages('dplyr')
+install.packages('geosphere')
+install.packages('stringr')
+
+library('dplyr')
+library('readr')
+library('geosphere')
+library('stringr')
+
+## 1.2 Loading Data
+df1 <- read_csv('/home/vitor/Repos/google_project/data/df_raw_cyclistic.csv')
+
+
+############################################################################
+
+# 2. DATA DESCRIPTION
+
+## 2.1 Overview
+head(df1)
+glimpse(df1)
+summary(df1)
+str(df1)
+
+## 2.2 Check and Fillout NA
+
+sum(is.na(df1$start_station_id))
+
+df1[5:8] <- list(NULL) # drop stations columns (14,5% NA)
+df1 <- na.omit(df1)
+
+df1 <- df1[order(df1$started_at),]
+
+## 2.3 Change Types
+
+  # Was not necessary #
+
+############################################################################
+
+# 3. FEATURE ENGINEERING
+
+df3 <- data.frame(df1)
+
+## 3.1 Date
+df3$date <- as.Date(df3$started_at)
+
+## 3.2 Month
+df3$month <- format(as.Date(df3$date), '%m')
+
+## 3.3 Day
+df3$day <- format(as.Date(df3$date), '%d')
+
+## 3.4 Year
+df3$year <- format(as.Date(df3$date), '%Y')
+
+## 3.5 Day of Week
+df3$day_of_week <- format(as.Date(df3$date), '%A')
+
+## 3.6 Duration (min)
+df3$duration <- difftime(df3$ended_at, df3$started_at, units='mins')
+df3$duration <- round(df3$duration, digit=2)
+
+## 3.7 Distance (km)
+df3$distance <- distGeo(matrix(c(df3$start_lng, df3$start_lat), ncol=2), matrix(c(df3$end_lng, df3$end_lat), ncol=2))/1000
+df3$distance <- round(df3$distance, digit=2)
+
+## 3.8 Speed (km/h)
+df3$speed = c(df3$distance)/as.numeric(c(df3$duration), units='hours')
+df3$speed <- round(df3$speed, digit=2)
+
+## 3.9 Station Location
+
+df3$start_lat <- round(df3$start_lat, digit=3)
+df3$start_lng <- round(df3$start_lng, digit=3)
+df3$end_lat <- round(df3$start_lat, digit=3)
+df3$end_lng <- round(df3$end_lng, digit=3)
+
+df3$start_station <- str_c(df3$start_lat, ' , ', df3$start_lng)
+df3$end_station <- str_c(df3$end_lat, ' , ', df3$end_lng)
+
+View(df3)
+
+# n_distinct(df3$end_station)
+
+############################################################################
+
+# 4. DATA FILTERING
+
+df4 <- data.frame(df3)
+
+## 4.1 Duration < 1 min
+df4 <- subset(df4, df4[15] >= 1 )
+
+## 4.2 Distance < 1m
+df4 <- subset(df4, df4[16] > 0.001)
+
+## 4.3 Distance > 100km (wrong records)
+df4 <- subset(df4, df4[16] < 100)
+
+## 4.2 Speed < 0 
+df4 <- subset(df4, df4[17] > 0)
+
+## 4.3 Speed > 50
+df4 <- subset(df4, df4[17] < 60)
+
+summary(df4)
+
+View(df4)
+str(df4)
+
+
+# 5. EXPORT DATA FRAME
+
+write.csv(df4,"/home/vitor/Repos/google_project/data/df_cyclistic_analysis.csv", row.names = FALSE)
+
